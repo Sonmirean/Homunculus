de98cb8736795a19496a826c0f54e1346e72a88b
cebem1nt <mineewarik@gmail.com>
2025-11-23 21:22:05 -0300

    Idk, just moving around

--- src/main.cpp
+++ src/main.cpp
@@ -8,48 +8,49 @@
 #include <stdexcept>
 #include <vector>
 
-#define sigmoid_func(z) (1 / (1 + std::exp(-(z))))
-
-template <typename T>
-bool perceptron(std::vector<T>& xs, std::vector<T>& ws, T bias) 
+inline double sigmoid(double x) 
 {
-    if (xs.size() != ws.size())
-        throw std::invalid_argument("Xs an weights must have same size");
-
-    T sum = 0;
-    for (std::size_t i = 0; i < xs.size(); i++)
-        sum += xs[i] * ws[i];
-
-    return sum + bias > 0;
+    return 1.0 / (1.0 + std::exp(x));
 }
 
-template <typename T>
-T sigmoid_neuron(std::vector<T>& xs, std::vector<T>& ws, T bias) 
+bool perceptron(std::vector<double>& xs, std::vector<double>& ws, double bias) 
 {
     if (xs.size() != ws.size())
         throw std::invalid_argument("Xs an weights must have same size");
 
-    T sum = 0;
+    double sum = bias;
     for (std::size_t i = 0; i < xs.size(); i++)
         sum += xs[i] * ws[i];
 
-    return sigmoid_func(sum + bias);
+    return sum > 0;
+}
+
+double sigmoid_neuron(std::vector<double>& xs, std::vector<double>& ws, double bias) 
+{
+    if (xs.size() != ws.size())
+        throw std::invalid_argument("Xs an weights must have same size");
+
+    double sum = bias;
+    for (std::size_t i = 0; i < xs.size(); i++)
+        sum += xs[i] * ws[i];
+
+    return sigmoid(sum);
 }
 
 int main() 
 {
     // For now tests go here
 
-    int bias = -1;
-    std::vector<int> xs = {10, 2, 1,  -12};
-    std::vector<int> ws = {1,  1, 11,  21};
+    double bias = -1;
+    std::vector<double> xs = {10, 2, 1,  -12};
+    std::vector<double> ws = {1,  1, 11,  21};
 
-    float bias2 = 1.234;
-    std::vector<float> xs2 = {112.247, 2.31, 1.98, -102.11};
-    std::vector<float> ws2 = {678.122, 1.24, 1.00,  1.56};
+    double bias2 = 1.234;
+    std::vector<double> xs2 = {112.247, 2.31, 1.98, -102.11};
+    std::vector<double> ws2 = {678.122, 1.24, 1.00,  1.56};
 
     bool is_active = perceptron(xs, ws, bias);
-    float coef = sigmoid_neuron(xs2, ws2, bias2);
+    double coef = sigmoid_neuron(xs2, ws2, bias2);
 
     std::cout << "(Perceptron) Is active: " << is_active << '\n';
     std::cout << "(Sigmoid) Coefficient: " << coef << '\n';
